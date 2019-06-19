363d83568b84b3f5df39c9cdc9ea33bee51a13ea
Sergii Iegorov <sergii.iegorov@globallogic.com>
2019-06-19 17:11:39 +0300

    Add Car service audio config
    Adds car_audio_cofiguration.xml as it's introduced as
    nesessary config file in Q and so that gets rid of
    obsolete functionality.
    
    Signed-off-by: Sergii Iegorov <sergii.iegorov@globallogic.com>
    Change-Id: I45f0355e4359a9f7bc6e15c092d66cf2bb666a91

--- car_audio.mk
+++ car_audio.mk
@@ -27,6 +27,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
-    vendor/renesas/hal/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
+    vendor/renesas/hal/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    vendor/renesas/hal/audio/car_audio_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/car_audio_configuration.xml
 
 DEVICE_PACKAGE_OVERLAYS += vendor/renesas/hal/audio/overlay
